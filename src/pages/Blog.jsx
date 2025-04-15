3fa2a9bd7ed62ea38926d472dec65104462af521
Abenezer <150917473+Abu388@users.noreply.github.com>
2025-04-15 21:48:24 +0300

    updated blog page

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,7 +1,9 @@
-import BlogMainContent from "../components/Blogmaincontent";
-import BlogSidebar from "../components/Blogsidebar";
-import CommentForm from "../components/commentform";
-// import   myImage from './Images/Header.jpg';
+import BlogmainContent from "../components/Blogmaincontent";
+import Blogsidebar from "../components/Blogsidebar"
+
+import Commentform from "../components/commentform"
+import Footer from '../components/Footer'
+
 
 const Blog = () => {
   return (
@@ -9,7 +11,7 @@ const Blog = () => {
       {/* Hero */}
       <div className="relative h-[400px] w-full">
         <img
-          src="/assets/blog-cover.jpg"
+          src="/assets/packageimg/adventure.jpg"
           alt="Blog Cover"
           className="w-full h-full object-cover"
         />
@@ -23,10 +25,10 @@ const Blog = () => {
       
       <div className="max-w-7xl mx-auto px-4 py-12 grid grid-cols-1 md:grid-cols-3 gap-8">
         <div className="md:col-span-2">
-          <BlogMainContent />
-          <CommentForm />
+          <BlogmainContent />
+          <Commentform />
         </div>
-        <BlogSidebar />
+        <Blogsidebar />
       </div>
       <Footer />
     </div>
